1057b2a541f330b70b8a8a00045de88814ee82ac
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-07-22 18:38:00 +0000

    like a ton

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express'); // latest
+const express = require('express');   // latest
 const fetch = require("node-fetch"); // 2.1.0
+const fs = require('fs');           // builtin
 
 const app = express();
 
 const root = "/home/runner/r/"
 
-app.get('/', (req, res) => {
+app.get(['/', /\/index.?h?t?m?l?/], (req, res) => {
   res.sendFile('index.html', {root: root})
 });
 
 app.get('/style.css', (req, res) => {
   res.sendFile('style.css', {root: root})
 });
+
+app.get([/\/p\/[0-9]+/, /\/projects?\/[0-9]+/], (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.set('Link', '<../../style.css>; rel="stylesheet"');
+  res.status(403).send('<div class="mono">403 Unauthorized</div>')
+})
 
 app.listen(3000, () => {
   console.log('server started');
