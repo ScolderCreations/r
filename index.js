1399d634ea21537370432476bdf6bed8b2232e0e
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-07-22 03:19:06 +0000

    gitignore and testing stuff

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const fetch = require("node-fetch"); // 2.1.0
 
 const app = express();
 
+const root = "/home/runner/r/"
+
 app.get('/', (req, res) => {
-  res.send('shovel knight')
+  res.sendFile('index.html', {root: root})
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile('style.css', {root: root})
 });
 
 app.listen(3000, () => {
   console.log('server started');
 });
-
-updateDB()
