71c2403ba5eb3cd2be17d84ee35b6b14b94d2ba5
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-07-22 01:22:05 +0000

    begin repurposing

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ const fetch = require("node-fetch"); // 2.1.0
 
 const app = express();
 
-var data = {};
-
-function updateDB() {
-  let id = (Math.random() * 715267356) + 500;
-}
-updateDB()
-
-setInterval(updateDB, 500000);
-
 app.get('/', (req, res) => {
   res.send('shovel knight')
 });
@@ -19,3 +10,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('server started');
 });
+
+updateDB()
